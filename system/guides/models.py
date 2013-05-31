30c9f4c99b4839278b459e1623b03f34298fc238
Vee <happierv@gmail.com>
2013-05-31 22:48:34 +0700

    Import log
    Change spaces to tab

--- system/guides/models.py
+++ system/guides/models.py
@@ -5,73 +5,73 @@ from django.template.defaultfilters import slugify
 from django.db.models import Max
 
 class Person(models.Model):
-    no = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    organization = models.CharField(max_length=50)
+	no = models.AutoField(primary_key=True)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	organization = models.CharField(max_length=50)
 
-    def filename(self, filename):
-        if self.no:
-            num = self.no
-        else:
-            max_number = Person.objects.all().aggregate(Max('no'))
-            if max_number['no__max']:
-                num = max_number['no__max']+1
-            else:
-                num = 1
-        fname, dot, extension = filename.rpartition('.')
-        return 'people/'+'%s.%s' % (num, extension)
+	def filename(self, filename):
+		if self.no:
+			num = self.no
+		else:
+			max_number = Person.objects.all().aggregate(Max('no'))
+			if max_number['no__max']:
+				num = max_number['no__max']+1
+			else:
+				num = 1
+		fname, dot, extension = filename.rpartition('.')
+		return 'people/'+'%s.%s' % (num, extension)
 
-    image = models.FileField(upload_to=filename)
+	image = models.FileField(upload_to=filename)
 
-    def save(self, *args, **kwargs):
-        # delete old file when replacing by updating the file
-        try:
-            curr_img = Person.objects.get(no=self.no)
-            if curr_img.image != self.image:
-                curr_img.image.delete(save=False)
-        except: pass # when new photo then we do nothing, normal case          
-        super(Person, self).save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		# delete old file when replacing by updating the file
+		try:
+			curr_img = Person.objects.get(no=self.no)
+			if curr_img.image != self.image:
+				curr_img.image.delete(save=False)
+		except: pass # when new photo then we do nothing, normal case          
+		super(Person, self).save(*args, **kwargs)
 
-    def __unicode__(self):
-        return u'%s %s %s' % (self.no, self.first_name, self.last_name)
-        
-    @models.permalink
-    def get_absolute_url(self):
-        return ('view_person', (), {'no': self.no})
+	def __unicode__(self):
+		return u'%s %s %s' % (self.no, self.first_name, self.last_name)
+		
+	@models.permalink
+	def get_absolute_url(self):
+		return ('view_person', (), {'no': self.no})
 
 class PersonAdmin(admin.ModelAdmin): 
-    #change_list_template = 'smuggler/change_list.html'
-    fieldsets = [('Name', {'fields': ('first_name', 'last_name')}),]
-    list_display = ('first_name', 'last_name')
-    search_fields = ['first_name', 'last_name']
-    #list_filter = ('first_name', 'last_name')
+	#change_list_template = 'smuggler/change_list.html'
+	fieldsets = [('Name', {'fields': ('first_name', 'last_name')}),]
+	list_display = ('first_name', 'last_name')
+	search_fields = ['first_name', 'last_name']
+	#list_filter = ('first_name', 'last_name')
 
 #admin.site.register(Person, PersonAdmin)
 admin.site.register(Person)
 
 class BannedPerson(models.Model):
-    name = models.OneToOneField(Person, related_name='is banned')
-    timestamp = models.DateTimeField(auto_now_add=True)
-    def __unicode__(self):
-        return self.name.first_name+' '+self.name.last_name
+	name = models.OneToOneField(Person, related_name='is banned')
+	timestamp = models.DateTimeField(auto_now_add=True)
+	def __unicode__(self):
+		return self.name.first_name+' '+self.name.last_name
 
 class BannedPersonAdmin(admin.ModelAdmin): 
-    list_display = ('name', 'timestamp')
-    list_filter = ['timestamp']
-    search_fields = ['name__first_name', 'name__last_name']
+	list_display = ('name', 'timestamp')
+	list_filter = ['timestamp']
+	search_fields = ['name__first_name', 'name__last_name']
 
 admin.site.register(BannedPerson, BannedPersonAdmin)
 
 class Log(models.Model):
-    name = models.ForeignKey(Person, related_name='logs')
-    timestamp = models.DateTimeField()
-    def __unicode__(self):
-        return self.name.first_name+' '+self.name.last_name
+	name = models.ForeignKey(Person, related_name='logs')
+	timestamp = models.DateTimeField()
+	def __unicode__(self):
+		return self.name.first_name+' '+self.name.last_name
 
 class LogAdmin(admin.ModelAdmin): 
-    list_display = ('name', 'timestamp')
-    list_filter = ['timestamp']
-    search_fields = ['name__first_name', 'name__last_name']
+	list_display = ('name', 'timestamp')
+	list_filter = ['timestamp']
+	search_fields = ['name__first_name', 'name__last_name']
 
 admin.site.register(Log, LogAdmin)
